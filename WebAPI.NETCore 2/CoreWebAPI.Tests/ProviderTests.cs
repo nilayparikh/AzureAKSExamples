599ea8a5ab598d8b4df747dd9e6494f6a079a12f
Nilay Parikh <nilay.parikh@ergosum.in>
2018-03-03 13:56:18 +0000

    integration tests.

---
+++ WebAPI.NETCore 2/CoreWebAPI.Tests/ProviderTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CoreWebAPI.Tests
+{
+    public class ProviderTests
+    {
+        [Fact]
+        public void ProviderTestCase1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
---
+++ WebAPI.NETCore 2/Tests/API.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,31 @@
+using RestSharp;
+using System;
+using Xunit;
+
+namespace API.IntegrationTests
+{
+    public class IntegrationTests
+    {
+
+        public RestClient consumerClient = default(RestClient);
+
+        public IntegrationTests()
+        {
+            consumerClient = new RestClient("http://webapi");
+        }
+
+        [Fact]
+        public void IntegrationTest1()
+        {
+            var inputId = 11;
+
+            var requestToProvider = new RestRequest("/api/Consumer/{id}", Method.GET);
+            requestToProvider.AddUrlSegment("id", inputId);
+
+            var responseFromProvider = consumerClient.Execute(requestToProvider);
+            var content = responseFromProvider.Content;
+
+            Assert.Contains(content, inputId.ToString());
+        }
+    }
+}
